6bdfbccc13a82930d10f6929dfedf8119c869f23
NIGELababy <124333944+nigelababy@users.noreply.github.com>
2024-10-01 22:41:47 -0600

    git config -- global user name "Nigel"

--- SpinFile.py
+++ SpinFile.py
@@ -1,3 +1,4 @@
+# https://github.com/NIGELababy/NIGELababy.git
 import string
 
 from Spinner import Spinner
@@ -5,15 +6,15 @@ from Spinner import Spinner
 
 def main():
     with open('essay.txt', 'r') as essay_file:
-        original_text = essay_file.read().lower()
+        my_original_text = essay_file.read().lower()
 
-    original_text = original_text.translate(str.maketrans('', '', string.punctuation))
+    my_original_text = my_original_text.translate(str.maketrans('', '', string.punctuation))
 
-    print("Original: ", original_text)
+    print("Original: ", my_original_text)
 
     spinner = Spinner('myresults.txt')
     for i in range(1, 4):
-        spinner_text = spinner.spinner_text(original_text)
+        spinner_text = spinner.spinner_text(my_original_text)
         print(f"Option {i}: ", spinner_text)
 
 
